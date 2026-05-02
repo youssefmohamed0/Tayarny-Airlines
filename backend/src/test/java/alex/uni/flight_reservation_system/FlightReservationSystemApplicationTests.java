2e68266b29d31a97681e14776d645c90ecd8da9f
youssef <youssefyoussef.m4@gmail.com>
2026-05-03 02:24:57 +0300

    Initialize Spring

---
+++ backend/src/test/java/alex/uni/flight_reservation_system/FlightReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.uni.flight_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
