b0efc536eade49ab60aa81dbc62edf602f10ed0d
youssef <youssefyoussef.m4@gmail.com>
2026-05-11 16:20:19 +0300

    feat: add exception handler for bad auth input

---
+++ backend/src/main/java/alex/uni/flight_reservation_system/modules/auth/dto/ApiResponse.java
@@ -0,0 +1,17 @@
+package alex.uni.flight_reservation_system.modules.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    private int statusCode;
+    private String message;
+    private Object data;
+
+}
