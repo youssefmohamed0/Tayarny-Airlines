9dbec564e26d92b5faf0cd58afdade4bd965e346
Mohammed-Hatem <mr.mohammedhatem@gmail.com>
2026-05-11 16:37:51 +0300

    allow test airplane endpoints

--- backend/src/main/java/alex/uni/flight_reservation_system/config/SecurityConfig.java
+++ backend/src/main/java/alex/uni/flight_reservation_system/config/SecurityConfig.java
@@ -56,7 +56,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                    auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/test/**").permitAll()
                                 .requestMatchers("/api/searchflights/**").permitAll()
                                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
                                 .requestMatchers("/api/user/**").hasRole("CUSTOMER")
