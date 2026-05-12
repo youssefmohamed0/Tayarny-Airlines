0b7f1844d971d36b9a2584fb03c98ca9c1bdb05a
youssef <youssefyoussef.m4@gmail.com>
2026-05-12 05:33:01 +0300

    fix: added missing function

--- backend/src/main/java/alex/uni/flight_reservation_system/modules/flights/FlightSeatStatusRepository.java
+++ backend/src/main/java/alex/uni/flight_reservation_system/modules/flights/FlightSeatStatusRepository.java
@@ -38,4 +38,6 @@ public interface FlightSeatStatusRepository extends JpaRepository<FlightSeatStat
     List<FlightSeatStatus> findByFlightIdAndSeatIdsForUpdate(
             @Param("flightId") UUID flightId,
             @Param("seatIds") List<UUID> seatIds);
+
+    List<FlightSeatStatus> findByFlight_Id(UUID flightId); // why use this not findByFlightId?
 }
